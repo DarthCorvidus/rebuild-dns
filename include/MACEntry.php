1711f1d8585df34510672f47c32649c000d90475
DarthCorvidus <github@vm01.telton.de>
2019-07-18 20:40:49 +0200

    MACEntry::getIPv6 creates an IPv6-Address out of a IPv6 prefix and it's MAC address via SLAAC.

--- include/MACEntry.php
+++ include/MACEntry.php
@@ -24,6 +24,25 @@ class MACEntry {
 		return $this->ip;
 	}
 
+	function getIPv6(string $prefix):string {
+		$exp = explode(":", strtolower($this->mac));
+		$first = str_pad(decbin(hexdec($exp[0])), 8, 0, STR_PAD_LEFT);
+		$first[6] = ($first[6]==1?0:1);
+		$exp[0] = str_pad(dechex(bindec($first)), 2, 0, STR_PAD_LEFT);
+		$final = array();
+		foreach($exp as $key => $value) {
+			if($key==3) {
+				$final[] = "ff";
+				$final[] = "fe";
+			}
+			$final[] = $value;
+		}
+		for($i=0;$i<count($final);$i = $i+2) {
+			$ipv6[] = $final[$i].$final[$i+1];
+		}
+	return $prefix.":".implode(":", $ipv6);
+	}
+	
 	public function getLongName():string {
 		return $this->longName;
 	}
